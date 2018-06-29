34a8d230f5cd67d0e3e2e1f24b0dc09defe74017
Femi-DD <femi.highsky@yahoo.com>
2018-06-29 01:22:11 +0100

    Updated Fetch Listener
    Listener now caches a valid response to a request

--- sw.js
+++ sw.js
@@ -15,3 +15,38 @@ self.addEventListener('install', (event) => {
     })
   );
 });
+
+self.addEventListener('fetch', (event) => {
+  console.log('[ServiceWorker] Fetching...');
+
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      if (response) {
+        return response;
+      } else {
+        let requestClone = event.request.clone();
+
+        fetch(requestClone).then((response) => {
+          if (response.status === 404 || !response) {
+            return response;
+          } else {
+            let responseClone = response.clone();
+
+            caches.open(newCacheName).then((cache) => {
+              cache.put(requestClone, responseClone);
+            });
+
+          }
+        }).catch((error) => {
+          console.log("[ServiceWorker][Error] : Error fetching and caching new request and its response" + error);
+        })
+
+      }
+      return response;
+    }).catch((error) => {
+      console.log("[ServiceWorker][Error] : " + error);
+    })
+
+  );
+
+});
